cdb45f51854f554261ff425d98eb231eb2efb040
umbralcalc <umbralcalc@gmail.com>
2023-03-16 08:46:03 +0000

    added Cox process

--- pkg/phenomena/cox_process.go
+++ pkg/phenomena/cox_process.go
@@ -1,1 +1,56 @@
 package phenomena
+
+import (
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+	"golang.org/x/exp/rand"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+)
+
+// CoxProcessIteration defines an iteration for a Cox process.
+type CoxProcessIteration struct {
+	unitUniformDist           *distuv.Uniform
+	rateProcessPartitionIndex int
+}
+
+func (c *CoxProcessIteration) Iterate(
+	otherParams *simulator.OtherParams,
+	partitionIndex int,
+	stateHistories []*simulator.StateHistory,
+	timestepsHistory *simulator.TimestepsHistory,
+) *simulator.State {
+	stateHistory := stateHistories[partitionIndex]
+	rateHistory := stateHistories[c.rateProcessPartitionIndex]
+	values := make([]float64, stateHistory.StateWidth)
+	for i := range values {
+		if rateHistory.Values.At(0, i) > (rateHistory.Values.At(0, i)+
+			(1.0/timestepsHistory.NextIncrement))*c.unitUniformDist.Rand() {
+			values[i] = stateHistory.Values.At(0, i) + 1.0
+		} else {
+			values[i] = stateHistory.Values.At(0, i)
+		}
+	}
+	return &simulator.State{
+		Values: mat.NewVecDense(
+			stateHistory.StateWidth,
+			values,
+		),
+		StateWidth: stateHistory.StateWidth,
+	}
+}
+
+// NewCoxProcessIteration creates a new CoxProcessIteration given a
+// seed and a partition index for the rate process.
+func NewCoxProcessIteration(
+	seed uint64,
+	rateProcessPartitionIndex int,
+) *CoxProcessIteration {
+	return &CoxProcessIteration{
+		unitUniformDist: &distuv.Uniform{
+			Min: 0.0,
+			Max: 1.0,
+			Src: rand.NewSource(seed),
+		},
+		rateProcessPartitionIndex: rateProcessPartitionIndex,
+	}
+}
--- pkg/phenomena/cox_process_test.go
+++ pkg/phenomena/cox_process_test.go
@@ -1,1 +1,44 @@
 package phenomena
+
+import (
+	"testing"
+
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+)
+
+func TestCoxProcess(t *testing.T) {
+	t.Run(
+		"test that the Cox process runs",
+		func(t *testing.T) {
+			settings := simulator.NewLoadSettingsConfigFromYaml(
+				"cox_process_config.yaml",
+			)
+			iterations := make([]simulator.Iteration, 0)
+			iterations = append(
+				iterations,
+				NewCoxProcessIteration(settings.Seeds[0], 1),
+			)
+			// this implements a Neyman-Scott process
+			iterations = append(
+				iterations,
+				NewPoissonProcessIteration(settings.Seeds[1]),
+			)
+			store := make([][][]float64, len(settings.StateWidths))
+			implementations := &simulator.LoadImplementationsConfig{
+				Iterations:      iterations,
+				OutputCondition: &simulator.EveryStepOutputCondition{},
+				OutputFunction:  &simulator.VariableStoreOutputFunction{Store: store},
+				TerminationCondition: &simulator.NumberOfStepsTerminationCondition{
+					MaxNumberOfSteps: 100,
+				},
+				TimestepFunction: &simulator.ConstantTimestepFunction{Stepsize: 1.0},
+			}
+			config := simulator.NewStochadexConfig(
+				settings,
+				implementations,
+			)
+			coordinator := simulator.NewPartitionCoordinator(config)
+			coordinator.Run()
+		},
+	)
+}
