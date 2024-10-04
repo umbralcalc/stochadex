c360754e99d8a6568eb629262037d3bc7fef3509
umbralcalc <umbralcalc@gmail.com>
2024-10-04 08:04:38 +0100

    Added a constant integration kernel

---
+++ pkg/kernels/constant.go
@@ -0,0 +1,28 @@
+package kernels
+
+import (
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+)
+
+// ConstantIntegrationKernel just returns 1.0 for every value.
+type ConstantIntegrationKernel struct{}
+
+func (c *ConstantIntegrationKernel) Configure(
+	partitionIndex int,
+	settings *simulator.Settings,
+) {
+}
+
+func (c *ConstantIntegrationKernel) SetParams(
+	params simulator.Params,
+) {
+}
+
+func (c *ConstantIntegrationKernel) Evaluate(
+	currentState []float64,
+	pastState []float64,
+	currentTime float64,
+	pastTime float64,
+) float64 {
+	return 1.0
+}
