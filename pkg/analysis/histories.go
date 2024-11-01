df0124138b4af8d1fda7b712bf83a55897a30934
umbralcalc <umbralcalc@gmail.com>
2024-11-01 05:27:35 +0000

    DataFram writing added

--- pkg/analysis/histories.go
+++ pkg/analysis/histories.go
@@ -1,6 +1,9 @@
 package analysis
 
 import (
+	"strconv"
+
+	"github.com/go-gota/gota/dataframe"
 	"github.com/umbralcalc/stochadex/pkg/simulator"
 )
 
@@ -10,4 +13,20 @@ import (
 type StateTimeHistories struct {
 	StateHistories   map[string]*simulator.StateHistory
 	TimestepsHistory *simulator.CumulativeTimestepsHistory
+}
+
+// GetDataFrame constructs a dataframe from the state history of a given
+// partition. A "time" column is also provided.
+func (s *StateTimeHistories) GetDataFrame(
+	partitionName string,
+) dataframe.DataFrame {
+	stateHistory := s.StateHistories[partitionName]
+	df := dataframe.LoadMatrix(stateHistory.Values)
+	df = dataframe.LoadMatrix(s.TimestepsHistory.Values).CBind(df)
+	cols := []string{"time"}
+	for i := 0; i < stateHistory.StateWidth; i++ {
+		cols = append(cols, strconv.Itoa(i))
+	}
+	df.SetNames(cols...)
+	return df
 }
---
+++ pkg/analysis/histories_test.go
@@ -0,0 +1,46 @@
+package analysis
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+	"gonum.org/v1/gonum/mat"
+)
+
+func TestStateTimeHistories(t *testing.T) {
+	t.Run(
+		"test that the state time histories functionality works",
+		func(t *testing.T) {
+			stateTimeHistories := &StateTimeHistories{
+				StateHistories: map[string]*simulator.StateHistory{
+					"test": {
+						Values:            mat.NewDense(3, 2, []float64{5, 4, 3, 2, 1, 0}),
+						NextValues:        []float64{6, 7},
+						StateWidth:        2,
+						StateHistoryDepth: 3,
+					},
+				},
+				TimestepsHistory: &simulator.CumulativeTimestepsHistory{
+					NextIncrement:     1,
+					Values:            mat.NewVecDense(3, []float64{236, 235, 234}),
+					CurrentStepNumber: 237,
+					StateHistoryDepth: 3,
+				},
+			}
+			df := stateTimeHistories.GetDataFrame("test")
+			value := df.Elem(0, 0)
+			if value.Float() != 236 {
+				t.Error("df conversion failed. value was: " + fmt.Sprintf("%f", value))
+			}
+			value = df.Elem(0, 1)
+			if value.Float() != 5 {
+				t.Error("df conversion failed. value was: " + fmt.Sprintf("%f", value))
+			}
+			value = df.Elem(0, 2)
+			if value.Float() != 4 {
+				t.Error("df conversion failed. value was: " + fmt.Sprintf("%f", value))
+			}
+		},
+	)
+}
