5ccf4f13891c958abceae8c5abe6f7389864d453
umbralcalc <umbralcalc@gmail.com>
2024-11-05 09:33:27 +0000

    Added a WriteStateTimeStorageToPostgresDb function

--- pkg/analysis/postgres.go
+++ pkg/analysis/postgres.go
@@ -1,97 +1,87 @@
 package analysis
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
-	"log"
-	"sync"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
+	"github.com/umbralcalc/stochadex/pkg/general"
 	"github.com/umbralcalc/stochadex/pkg/simulator"
 )
 
-// openTableConnection connects to PostgreSQL database or creates it
+// PostgresDb is a struct which can be configured to define interactions
+// with a PostgresSQL database.
+type PostgresDb struct {
+	User      string
+	Password  string
+	Dbname    string
+	TableName string
+	db        *sql.DB
+}
+
+// OpenTableConnection connects to the PostgreSQL database or creates it
 // if it doesn't exist.
-func openTableConnection(config *PostgresDbConfig) (*sql.DB, error) {
+func (p *PostgresDb) OpenTableConnection() error {
+	var err error
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		config.User, config.Password, config.Dbname)
-	db, err := sql.Open("postgres", connStr)
+		p.User, p.Password, p.Dbname)
+	p.db, err = sql.Open("postgres", connStr)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	// Generate column declarations based on storage
-	colDeclarations := ""
-	for partition, rowSize := range config.RowSizeByPartitionName {
-		for i := 0; i < rowSize; i++ {
-			colDeclarations += fmt.Sprintf("%s%d DOUBLE PRECISION NOT NULL, ",
-				partition, i)
-		}
-	}
-
-	// Write and execute the generated query string
 	createTableQuery := fmt.Sprintf(`
 	CREATE TABLE IF NOT EXISTS %s (
-		id SERIAL PRIMARY KEY,
+		partition_name TEXT NOT NULL,
 		time DOUBLE PRECISION NOT NULL,
-		%s
-	);`, config.TableName, colDeclarations)
-	_, err = db.Exec(createTableQuery)
+		state FLOAT8[] NOT NULL,
+		PRIMARY KEY (partition_name, time)
+	);`, p.TableName)
+	_, err = p.db.Exec(createTableQuery)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	return db, nil
+	return nil
 }
 
-// insertEmptyRow inserts a new empty row and returns the
-// ID of the created row.
-func insertEmptyRowQuery(db *sql.DB, tableName string) (int64, error) {
-	var rowId int64
-	err := db.QueryRow(fmt.Sprintf(
-		"INSERT INTO %s DEFAULT VALUES RETURNING id",
-		tableName,
-	)).Scan(&rowId)
-	return rowId, err
-}
-
-// updateColumnGroup updates a specific group of columns
-// for a given row ID.
-func updateColumnGroupQuery(
-	ctx context.Context,
-	db *sql.DB,
-	tableName string,
-	rowId int64,
-	columnValues map[string]interface{},
+// WriteState writes a new partition state value to the database.
+func (p *PostgresDb) WriteState(
+	partitionName string,
+	time float64,
+	state []float64,
 ) error {
-	setClauses := ""
-	args := []interface{}{rowId}
-	i := 2
-	for column, value := range columnValues {
-		setClauses += fmt.Sprintf("%s = $%d, ", column, i)
-		args = append(args, value)
-		i++
+	tx, err := p.db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to start transaction: %v", err)
 	}
-	setClauses = setClauses[:len(setClauses)-2]
-	query := fmt.Sprintf("UPDATE %s SET %s WHERE id = $1;",
-		tableName, setClauses)
-	_, err := db.ExecContext(ctx, query, args...)
-	return err
+	defer tx.Rollback()
+	stmt, err := tx.Prepare(fmt.Sprintf(`
+        INSERT INTO %s (partition_name, time, state)
+        VALUES ($1, $2, $3)
+        ON CONFLICT (partition_name, time)
+        DO UPDATE SET state = EXCLUDED.state
+    `, p.TableName))
+	if err != nil {
+		return fmt.Errorf("failed to prepare statement: %v", err)
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(partitionName, time, pq.Array(state))
+	if err != nil {
+		return fmt.Errorf(
+			"failed to execute statement for %s: %v",
+			partitionName,
+			err,
+		)
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit transaction: %v", err)
+	}
+	return nil
 }
 
-// PostgresDbConfig
-type PostgresDbConfig struct {
-	User                   string
-	Password               string
-	Dbname                 string
-	TableName              string
-	RowSizeByPartitionName map[string]int
-}
-
-// PostgresDbOutputFunction
+// PostgresDbOutputFunction writes the data from the simulation to
+// a PostgresSQL database when the simulator.OutputCondition is met.
 type PostgresDbOutputFunction struct {
-	db *sql.DB
+	db *PostgresDb
 }
 
 func (p *PostgresDbOutputFunction) Output(
@@ -99,54 +89,56 @@ func (p *PostgresDbOutputFunction) Output(
 	state []float64,
 	cumulativeTimesteps float64,
 ) {
-
+	p.db.WriteState(partitionName, cumulativeTimesteps, state)
 }
 
 // NewPostgresDbOutputFunction creates a new PostgresDbOutputFunction.
 func NewPostgresDbOutputFunction(
-	config *PostgresDbConfig,
+	db *PostgresDb,
 ) *PostgresDbOutputFunction {
-	db, err := openTableConnection(config)
+	err := db.OpenTableConnection()
 	if err != nil {
 		panic(err)
 	}
 	return &PostgresDbOutputFunction{db: db}
 }
 
-// asyncWriteColumnGroups performs concurrent column group updates
-func asyncWriteColumnGroups(db *sql.DB, tableName string, columnGroups []map[string]interface{}) error {
-	// Insert an empty row and get the new row ID
-	rowId, err := insertEmptyRowQuery(db, tableName)
-	if err != nil {
-		return fmt.Errorf("failed to insert empty row: %v", err)
-	}
-
-	// Create a WaitGroup to synchronize goroutines
-	var wg sync.WaitGroup
-	ctx := context.Background()
-
-	// Iterate over each column group and launch a goroutine for each
-	for _, columnGroup := range columnGroups {
-		wg.Add(1)
-		go func(colGroup map[string]interface{}) {
-			defer wg.Done()
-			if err := updateColumnGroupQuery(ctx, db, tableName, rowId, colGroup); err != nil {
-				log.Printf("failed to update column group: %v", err)
-			}
-		}(columnGroup)
-	}
-
-	// Wait for all goroutines to complete
-	wg.Wait()
-	return nil
-}
-
-// WriteStateTimeStorageToPostgres writes all of the data
+// WriteStateTimeStorageToPostgresDb writes all of the data
 // in the state time storage to a PostgreSQL database.
-func WriteStateTimeStorageToPostgres(
-	config *PostgresDbConfig,
+func WriteStateTimeStorageToPostgresDb(
+	db *PostgresDb,
 	storage *simulator.StateTimeStorage,
 ) {
-	// outputFunction := NewPostgresDbOutputFunction(config)
-	_ = NewPostgresDbOutputFunction(config)
+	generator := simulator.NewConfigGenerator()
+	times := storage.GetTimes()
+	generator.SetSimulation(
+		&simulator.SimulationConfig{
+			OutputCondition: &simulator.EveryStepOutputCondition{},
+			OutputFunction:  NewPostgresDbOutputFunction(db),
+			TerminationCondition: &simulator.NumberOfStepsTerminationCondition{
+				MaxNumberOfSteps: len(times) - 1,
+			},
+			TimestepFunction: &general.FromStorageTimestepFunction{
+				Data: times,
+			},
+			InitTimeValue: times[0],
+		},
+	)
+	for _, name := range storage.GetNames() {
+		data := storage.GetValues(name)
+		generator.SetPartition(
+			&simulator.PartitionConfig{
+				Name: name,
+				Iteration: &general.FromStorageIteration{
+					Data: data,
+				},
+				Params:            simulator.NewParams(make(map[string][]float64)),
+				InitStateValues:   data[0],
+				StateHistoryDepth: 1,
+				Seed:              0,
+			},
+		)
+	}
+	coordinator := simulator.NewPartitionCoordinator(generator.GenerateConfigs())
+	coordinator.Run()
 }
