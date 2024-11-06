115268a6c0486d4f5f6036387b758367e5f9d9c1
umbralcalc <umbralcalc@gmail.com>
2024-11-06 09:21:40 +0000

    Created a new from partition method

--- pkg/analysis/partitions.go
+++ pkg/analysis/partitions.go
@@ -1,1 +1,30 @@
 package analysis
+
+import (
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+)
+
+// NewStateTimeStorageFromPartition generates a new simulator.StateTimeStorage
+// by running a simulation with the specified partition configured.
+func NewStateTimeStorageFromPartition(
+	partition *simulator.PartitionConfig,
+	termination simulator.TerminationCondition,
+	timestep simulator.TimestepFunction,
+	initTime float64,
+) *simulator.StateTimeStorage {
+	generator := simulator.NewConfigGenerator()
+	storage := simulator.NewStateTimeStorage()
+	generator.SetSimulation(&simulator.SimulationConfig{
+		OutputCondition: &simulator.EveryStepOutputCondition{},
+		OutputFunction: &simulator.StateTimeStorageOutputFunction{
+			Store: storage,
+		},
+		TerminationCondition: termination,
+		TimestepFunction:     timestep,
+		InitTimeValue:        initTime,
+	})
+	generator.SetPartition(partition)
+	coordinator := simulator.NewPartitionCoordinator(generator.GenerateConfigs())
+	coordinator.Run()
+	return storage
+}
--- pkg/analysis/partitions_test.go
+++ pkg/analysis/partitions_test.go
@@ -1,1 +1,44 @@
 package analysis
+
+import (
+	"testing"
+
+	"github.com/umbralcalc/stochadex/pkg/general"
+	"github.com/umbralcalc/stochadex/pkg/simulator"
+)
+
+func TestStorageFromPartition(t *testing.T) {
+	t.Run(
+		"test that creating a new storage from partition works",
+		func(t *testing.T) {
+			storage := NewStateTimeStorageFromPartition(
+				&simulator.PartitionConfig{
+					Name:              "test",
+					Iteration:         &general.ConstantValuesIteration{},
+					Params:            simulator.NewParams(make(map[string][]float64)),
+					InitStateValues:   []float64{1.0, 2.0, 3.0},
+					StateHistoryDepth: 1,
+					Seed:              0,
+				},
+				&simulator.NumberOfStepsTerminationCondition{
+					MaxNumberOfSteps: 100,
+				},
+				&simulator.ConstantTimestepFunction{
+					Stepsize: 1.0,
+				},
+				0.0,
+			)
+			times := storage.GetTimes()
+			for i, values := range storage.GetValues("test") {
+				if values[0] != 1.0 ||
+					values[1] != 2.0 ||
+					values[2] != 3.0 {
+					t.Errorf("values not expected")
+				}
+				if times[i] != float64(i+1) {
+					t.Errorf("time not expected")
+				}
+			}
+		},
+	)
+}
