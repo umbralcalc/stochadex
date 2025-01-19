4e67326861613dbaab146e358243e5ff884e81dd
umbralcalc <umbralcalc@gmail.com>
2025-01-19 10:57:12 +0000

    Configuration additions to the likelihood comparison partition function

--- pkg/analysis/likelihood.go
+++ pkg/analysis/likelihood.go
@@ -9,9 +9,10 @@ import (
 // AppliedLikelihoodComparison is the base configuration for a rolling
 // comparison between a referenced dataset and referenced likelihood model.
 type AppliedLikelihoodComparison struct {
-	Name  string
-	Data  DataRef
-	Model inference.LikelihoodDistribution
+	Name       string
+	Data       DataRef
+	Model      inference.LikelihoodDistribution
+	WindowSize int
 }
 
 // NewLikelihoodComparisonPartition creates a new PartitionConfig for
@@ -21,14 +22,32 @@ func NewLikelihoodComparisonPartition(
 	storage *simulator.StateTimeStorage,
 ) *simulator.PartitionConfig {
 	generator := simulator.NewConfigGenerator()
+	generator.SetSimulation(&simulator.SimulationConfig{
+		OutputCondition: &simulator.NilOutputCondition{},
+		OutputFunction:  &simulator.NilOutputFunction{},
+		TerminationCondition: &simulator.NumberOfStepsTerminationCondition{
+			MaxNumberOfSteps: applied.WindowSize,
+		},
+		// TODO: Find a way to pass in the correct times from the data!
+		// TimestepFunction: &simulator.ConstantTimestepFunction{},
+		// InitTimeValue: 0.0,
+	})
+	params := simulator.NewParams(map[string][]float64{
+		"cumulative":    {1},
+		"burn_in_steps": {float64(applied.WindowSize)},
+	})
 	generator.SetPartition(&simulator.PartitionConfig{
-		Name: "",
+		Name: "comparison",
 		Iteration: &inference.DataComparisonIteration{
 			Likelihood: applied.Model,
 		},
+		Params:            params,
+		InitStateValues:   []float64{0.0},
+		StateHistoryDepth: 1,
+		Seed:              0,
 	})
 	return &simulator.PartitionConfig{
-		Name: "",
+		Name: applied.Name,
 		Iteration: general.NewEmbeddedSimulationRunIteration(
 			generator.GenerateConfigs(),
 		),
