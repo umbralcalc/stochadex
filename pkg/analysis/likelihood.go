374a4a79eb163cb1dec628dabc754b04b0e4e2c7
umbralcalc <umbralcalc@gmail.com>
2025-02-09 11:03:38 +0000

    Added posterior estimation tool

--- pkg/analysis/likelihood.go
+++ pkg/analysis/likelihood.go
@@ -44,12 +44,12 @@ type AppliedLikelihoodComparison struct {
 	Window WindowedPartitions
 }
 
-// newLikelihoodComparisonGenerator creates a new config generator for
+// NewLikelihoodComparisonPartition creates a new PartitionConfig for
 // a rolling likelihood comparison.
-func newLikelihoodComparisonGenerator(
+func NewLikelihoodComparisonPartition(
 	applied AppliedLikelihoodComparison,
 	storage *simulator.StateTimeStorage,
-) *simulator.ConfigGenerator {
+) *simulator.PartitionConfig {
 	generator := simulator.NewConfigGenerator()
 	generator.SetSimulation(&simulator.SimulationConfig{
 		OutputCondition: &simulator.NilOutputCondition{},
@@ -61,6 +61,14 @@ func newLikelihoodComparisonGenerator(
 		TimestepFunction: &simulator.ConstantTimestepFunction{Stepsize: 1.0},
 		InitTimeValue:    0.0,
 	})
+	if applied.Window.Partitions != nil {
+		for _, partition := range applied.Window.Partitions {
+			generator.SetPartition(partition)
+		}
+	}
+	simInitStateValues := make([]float64, 0)
+	simParamsAsPartitions := make(map[string][]string)
+	simParamsFromUpstream := make(map[string]simulator.NamedUpstreamConfig)
 	for _, ref := range applied.Window.Data {
 		initStateValues := ref.GetIndexFromStorage(storage, 0)
 		generator.SetPartition(&simulator.PartitionConfig{
@@ -71,11 +79,14 @@ func newLikelihoodComparisonGenerator(
 			StateHistoryDepth: 1,
 			Seed:              0,
 		})
-	}
-	if applied.Window.Partitions != nil {
-		for _, partition := range applied.Window.Partitions {
-			generator.SetPartition(partition)
-		}
+		simInitStateValues = append(simInitStateValues, initStateValues...)
+		simParamsAsPartitions[ref.PartitionName+"/state_memory_partition"] =
+			[]string{ref.PartitionName}
+		simParamsFromUpstream[ref.PartitionName+"/latest_data_values"] =
+			simulator.NamedUpstreamConfig{
+				Upstream: ref.PartitionName,
+				Indices:  ref.ValueIndices,
+			}
 	}
 	applied.Model.Init()
 	applied.Model.Params.Set("cumulative", []float64{1})
@@ -97,30 +108,6 @@ func newLikelihoodComparisonGenerator(
 		StateHistoryDepth:  1,
 		Seed:               0,
 	})
-	return generator
-}
-
-// NewLikelihoodComparisonPartition creates a new PartitionConfig for
-// a rolling likelihood comparison.
-func NewLikelihoodComparisonPartition(
-	applied AppliedLikelihoodComparison,
-	storage *simulator.StateTimeStorage,
-) *simulator.PartitionConfig {
-	generator := newLikelihoodComparisonGenerator(applied, storage)
-	simInitStateValues := make([]float64, 0)
-	simParamsAsPartitions := make(map[string][]string)
-	simParamsFromUpstream := make(map[string]simulator.NamedUpstreamConfig)
-	for _, ref := range applied.Window.Data {
-		initStateValues := ref.GetIndexFromStorage(storage, 0)
-		simInitStateValues = append(simInitStateValues, initStateValues...)
-		simParamsAsPartitions[ref.PartitionName+"/state_memory_partition"] =
-			[]string{ref.PartitionName}
-		simParamsFromUpstream[ref.PartitionName+"/latest_data_values"] =
-			simulator.NamedUpstreamConfig{
-				Upstream: ref.PartitionName,
-				Indices:  ref.ValueIndices,
-			}
-	}
 	simInitStateValues = append(simInitStateValues, 0.0)
 	simParams := simulator.NewParams(map[string][]float64{
 		"burn_in_steps": {float64(applied.Window.Depth)},
@@ -139,49 +126,125 @@ func NewLikelihoodComparisonPartition(
 	}
 }
 
-// AppliedInference is the base configuration for an online
-// inference of a simulation (specified by partition configs) from a
-// referenced dataset.
-type AppliedInference struct {
-	Name string
+// PosteriorEstimationNames is a collection of the names given to
+// partitions used in the AppliedPosteriorEstimation.
+type PosteriorEstimationNames struct {
+	LogNorm    string
+	Mean       string
+	Covariance string
+	Sampler    string
 }
 
-// NewSimulationInferencePartition creates a new PartitionConfig for
-// an online simulation inference.
-func NewSimulationInferencePartition(
-	comparison AppliedLikelihoodComparison,
-	inference AppliedInference,
-	storage *simulator.StateTimeStorage,
-) *simulator.PartitionConfig {
-	generator := newLikelihoodComparisonGenerator(comparison, storage)
-	simInitStateValues := make([]float64, 0)
-	simParamsAsPartitions := make(map[string][]string)
-	simParamsFromUpstream := make(map[string]simulator.NamedUpstreamConfig)
-	for _, ref := range comparison.Window.Data {
-		initStateValues := ref.GetIndexFromStorage(storage, 0)
-		simInitStateValues = append(simInitStateValues, initStateValues...)
-		simParamsAsPartitions[ref.PartitionName+"/state_memory_partition"] =
-			[]string{ref.PartitionName}
-		simParamsFromUpstream[ref.PartitionName+"/latest_data_values"] =
-			simulator.NamedUpstreamConfig{
-				Upstream: ref.PartitionName,
-				Indices:  ref.ValueIndices,
-			}
+// PosteriorDefaults is a collection of the defaults given to
+// partitions used in the AppliedPosteriorEstimation.
+type PosteriorDefaults struct {
+	LogNorm    float64
+	Mean       []float64
+	Covariance []float64
+	Sampler    []float64
+}
+
+// AppliedPosteriorEstimation is the base configuration for an online
+// inference of a simulation (specified by partition configs) from a
+// referenced dataset.
+type AppliedPosteriorEstimation struct {
+	Names         PosteriorEstimationNames
+	Defaults      PosteriorDefaults
+	LikelihoodRef DataRef
+	PastDiscount  float64
+	Seed          uint64
+}
+
+// NewPosteriorEstimationPartitions creates a set of PartitionConfigs for
+// an online posterior estimation process using rolling statistics.
+func NewPosteriorEstimationPartitions(
+	applied AppliedPosteriorEstimation,
+) []*simulator.PartitionConfig {
+	loglikeIndices := make([]float64, 0)
+	loglikePartitions := make([]string, 0)
+	paramPartitions := make([]string, 0)
+	if applied.LikelihoodRef.ValueIndices == nil {
+		panic("must set LikelihoodRef.ValueIndices to use posterior estimation")
 	}
-	simInitStateValues = append(simInitStateValues, 0.0)
-	simParams := simulator.NewParams(map[string][]float64{
-		"burn_in_steps": {float64(comparison.Window.Depth)},
+	for _, index := range applied.LikelihoodRef.ValueIndices {
+		loglikeIndices = append(loglikeIndices, float64(index))
+		loglikePartitions = append(
+			loglikePartitions,
+			applied.LikelihoodRef.PartitionName,
+		)
+		paramPartitions = append(
+			paramPartitions,
+			applied.Names.Sampler,
+		)
+	}
+	partitions := make([]*simulator.PartitionConfig, 0)
+	logNormParams := simulator.NewParams(make(map[string][]float64))
+	logNormParams.Set("loglike_indices", loglikeIndices)
+	logNormParams.Set(
+		"past_discounting_factor",
+		[]float64{applied.PastDiscount},
+	)
+	partitions = append(partitions, &simulator.PartitionConfig{
+		Name:      applied.Names.LogNorm,
+		Iteration: &inference.PosteriorLogNormalisationIteration{},
+		Params:    logNormParams,
+		ParamsAsPartitions: map[string][]string{
+			"loglike_partitions": loglikePartitions,
+		},
+		InitStateValues:   []float64{applied.Defaults.LogNorm},
+		StateHistoryDepth: 1,
+		Seed:              0,
 	})
-	return &simulator.PartitionConfig{
-		Name: inference.Name,
-		Iteration: general.NewEmbeddedSimulationRunIteration(
-			generator.GenerateConfigs(),
-		),
-		Params:             simParams,
-		ParamsAsPartitions: simParamsAsPartitions,
-		ParamsFromUpstream: simParamsFromUpstream,
-		InitStateValues:    simInitStateValues,
-		StateHistoryDepth:  1,
-		Seed:               0,
-	}
+	meanParams := simulator.NewParams(make(map[string][]float64))
+	meanParams.Set("loglike_indices", loglikeIndices)
+	partitions = append(partitions, &simulator.PartitionConfig{
+		Name:      applied.Names.Mean,
+		Iteration: &inference.PosteriorMeanIteration{},
+		Params:    meanParams,
+		ParamsAsPartitions: map[string][]string{
+			"loglike_partitions": loglikePartitions,
+			"param_partitions":   paramPartitions,
+		},
+		ParamsFromUpstream: map[string]simulator.NamedUpstreamConfig{
+			"posterior_log_normalisation": {Upstream: applied.Names.LogNorm},
+		},
+		InitStateValues:   applied.Defaults.Mean,
+		StateHistoryDepth: 1,
+		Seed:              0,
+	})
+	covParams := simulator.NewParams(make(map[string][]float64))
+	covParams.Set("loglike_indices", loglikeIndices)
+	partitions = append(partitions, &simulator.PartitionConfig{
+		Name:      applied.Names.Covariance,
+		Iteration: &inference.PosteriorCovarianceIteration{},
+		Params:    covParams,
+		ParamsAsPartitions: map[string][]string{
+			"loglike_partitions": loglikePartitions,
+			"param_partitions":   paramPartitions,
+		},
+		ParamsFromUpstream: map[string]simulator.NamedUpstreamConfig{
+			"posterior_log_normalisation": {Upstream: applied.Names.LogNorm},
+			"mean":                        {Upstream: applied.Names.Mean},
+		},
+		InitStateValues:   applied.Defaults.Covariance,
+		StateHistoryDepth: 1,
+		Seed:              0,
+	})
+	samplerParams := simulator.NewParams(make(map[string][]float64))
+	samplerParams.Set("default_covariance", applied.Defaults.Covariance)
+	partitions = append(partitions, &simulator.PartitionConfig{
+		Name: applied.Names.Sampler,
+		Iteration: &inference.DataGenerationIteration{
+			Likelihood: &inference.NormalLikelihoodDistribution{},
+		},
+		Params: samplerParams,
+		ParamsFromUpstream: map[string]simulator.NamedUpstreamConfig{
+			"mean":              {Upstream: applied.Names.Mean},
+			"covariance_matrix": {Upstream: applied.Names.Covariance},
+		},
+		InitStateValues:   applied.Defaults.Sampler,
+		StateHistoryDepth: 1,
+		Seed:              applied.Seed,
+	})
+	return partitions
 }
--- pkg/analysis/likelihood_test.go
+++ pkg/analysis/likelihood_test.go
@@ -64,5 +64,85 @@ func TestLikelihood(t *testing.T) {
 			}
 		},
 	)
-
+	t.Run(
+		"test that the posterior estimation works",
+		func(t *testing.T) {
+			storage := NewStateTimeStorageFromPartitions(
+				[]*simulator.PartitionConfig{
+					{
+						Name: "test_data",
+						Iteration: &inference.DataGenerationIteration{
+							Likelihood: &inference.NormalLikelihoodDistribution{},
+						},
+						Params: simulator.NewParams(map[string][]float64{
+							"mean":              {1.8, 5.0},
+							"covariance_matrix": {2.5, 0.0, 0.0, 9.0},
+						}),
+						InitStateValues:   []float64{1.3, 8.3},
+						StateHistoryDepth: 1,
+						Seed:              123,
+					},
+				},
+				&simulator.NumberOfStepsTerminationCondition{MaxNumberOfSteps: 500},
+				&simulator.ConstantTimestepFunction{Stepsize: 1.0},
+				0.0,
+			)
+			params := simulator.NewParams(make(map[string][]float64))
+			params.Set("mean", []float64{1.8, 5.0})
+			params.Set("covariance_matrix", []float64{2.5, 0.0, 0.0, 9.0})
+			likePartition := NewLikelihoodComparisonPartition(
+				AppliedLikelihoodComparison{
+					Name: "test_likelihood",
+					Model: ParameterisedModel{
+						Likelihood: &inference.NormalLikelihoodDistribution{},
+						Params:     params,
+					},
+					Data: DataRef{PartitionName: "test_data"},
+					Window: WindowedPartitions{
+						Data:  []DataRef{{PartitionName: "test_data"}},
+						Depth: 200,
+					},
+				},
+				storage,
+			)
+			partitions := NewPosteriorEstimationPartitions(
+				AppliedPosteriorEstimation{
+					Names: PosteriorEstimationNames{
+						LogNorm:    "test_post_log_norm",
+						Mean:       "test_post_mean",
+						Covariance: "test_post_cov",
+						Sampler:    "test_post_sampler",
+					},
+					Defaults: PosteriorDefaults{
+						LogNorm:    0.0,
+						Mean:       []float64{1.8, 5.0},
+						Covariance: []float64{2.5, 0.0, 0.0, 9.0},
+						Sampler:    []float64{1.8, 5.0},
+					},
+					LikelihoodRef: DataRef{
+						PartitionName: "test_likelihood",
+						ValueIndices:  []int{2},
+					},
+					PastDiscount: 1.0,
+					Seed:         1234,
+				},
+			)
+			partitions = append(
+				[]*simulator.PartitionConfig{likePartition},
+				partitions...,
+			)
+			storage = AddPartitionsToStateTimeStorage(
+				storage,
+				partitions,
+				map[string]int{"test_data": 200},
+			)
+			for _, name := range storage.GetNames() {
+				for _, values := range storage.GetValues(name) {
+					if floats.HasNaN(values) {
+						t.Error("partition " + name + " values have NaN")
+					}
+				}
+			}
+		},
+	)
 }
