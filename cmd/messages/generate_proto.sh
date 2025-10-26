197a8c2f1b7ccddb9c257cb2f10426bd96d76826
Robert Hardwick <umbralcalc@gmail.com>
2025-10-26 10:05:40 +0000

    extended suite of message languages

--- cmd/messages/generate_proto.sh
+++ cmd/messages/generate_proto.sh
@@ -1,3 +1,5 @@
 protoc -I=. \
     --go_out=$(pwd) \
-    ./cmd/messages/partition_state.proto
+    --js_out=library=./cmd/messages/partition_state_pb,binary:. \
+    ./cmd/messages/partition_state.proto;
+protoc --python_out=. ./cmd/messages/partition_state.proto;
